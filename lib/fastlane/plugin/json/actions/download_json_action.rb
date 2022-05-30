0f19b536e899f53ef4cdc46b392e1be8bf4895cd
Thang Nguyen <thang.nguyencao@gmail.com>
2022-05-31 01:13:24 +0700

    Supported basic authentication

--- lib/fastlane/plugin/json/actions/download_json_action.rb
+++ lib/fastlane/plugin/json/actions/download_json_action.rb
@@ -9,13 +9,26 @@ module Fastlane
       def self.run(params)
         json_url = params[:json_url]
         @is_verbose = params[:verbose]
+        username = params[:username]
+        password = params[:password]
+
         uri = URI(json_url)
         print_params(params) if @is_verbose
         puts("Downloading json from #{uri}") if @is_verbose
 
         begin
-          response = Net::HTTP.get_response(uri)
-          JSON.parse(response.body, symbolize_names: true)
+          Net::HTTP.start(uri.host, uri.port,
+                          use_ssl: uri.scheme == 'https',
+                          verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+
+            request = Net::HTTP::Get.new(uri.request_uri)
+            if !username.nil? && !username.empty? && !password.nil? && !password.empty?
+              request.basic_auth(params[:username], params[:password])
+            end
+
+            response = http.request(request)
+            JSON.parse(response.body, symbolize_names: true)
+          end
         rescue JSON::ParserError
           puts_error!("Downloaded json has invalid content ❌")
         rescue => exception
@@ -45,6 +58,16 @@ module Fastlane
                                        verify_block: proc do |value|
                                          UI.user_error!("You must set json_url pointing to a json file") unless value && !value.empty?
                                        end),
+          FastlaneCore::ConfigItem.new(key: :username,
+                                       description: "Basic auth username to download",
+                                       optional: true,
+                                       is_string: true,
+                                       default_value: nil),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       description: "Basic auth password to download",
+                                       optional: true,
+                                       is_string: true,
+                                       default_value: nil),
           FastlaneCore::ConfigItem.new(key: :verbose,
                                        description: "verbose",
                                        optional: true,
@@ -66,7 +89,7 @@ module Fastlane
       end
 
       def self.authors
-        ["Martin Gonzalez"]
+        ["Martin Gonzalez", "Thang Nguyen"]
       end
 
       def self.is_supported?(_platform)
